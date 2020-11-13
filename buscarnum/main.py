132f886024b013b8e1571ec82a16d45de9609a38
Sebastianrza <45112547+Sebastianrza@users.noreply.github.com>
2020-11-13 12:46:10 +0100

    Update main.py

--- buscarnum/main.py
+++ buscarnum/main.py
@@ -2,27 +2,26 @@ import hashlib
 import re
 
 if __name__ == "__main__":
-   y = 921
+   nonce = 0
    T = True
-   f = open("/home/Sebastianrza/fichero1.txt", 'w')
+   file = open("/home/Sebastianrza/fichero1.txt", 'w')
    while T:
       inputFile = open("/home/Sebastianrza/fichero1.txt")
       readFile = inputFile.read()
-      h = hashlib.sha1(readFile.encode())
-      print(h.hexdigest())
-      if (re.match("[0]{2}", h.hexdigest())):
-         print("ganaste wey")
-         print(y)
-         f.close()
+      hash = hashlib.sha1(readFile.encode())
+      print(hash.hexdigest())
+      if (re.match("[0]{2}", hash.hexdigest())):
+         print(nonce)
+         file.close()
          T = False
       else:
-         f = open("/home/Sebastianrza/fichero1.txt", 'w')
-         y=y+1
-         f.write('Nombre y Apellidos: Sebastian Rafael Zambrano Azuaje\n')
-         f.write('Hash Previo: 008AD9491CABD7BD6291D32503647CC191EECC8F\n')
-         f.write('Nonce: '+str(y))
-         f.close()
-         print(y)
+         file = open("/home/Sebastianrza/fichero1.txt", 'w')
+         nonce=nonce+1
+         file.write('Nombre y Apellidos: Sebastian Rafael Zambrano Azuaje\n')
+         file.write('Hash Previo: 008AD9491CABD7BD6291D32503647CC191EECC8F\n')
+         file.write('Nonce: '+str(nonce))
+         file.close()
+       
 
 
 
